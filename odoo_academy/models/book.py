5633084ea92bb440b3c8e525843a6d1cd1e2098f
chra-odoo <chra@odoo.com>
2021-08-03 01:18:38 +0000

    [ADD] add demo data

--- odoo_academy/models/book.py
+++ odoo_academy/models/book.py
@@ -2,17 +2,15 @@
 
 from odoo import models, fields, api
 
-class Course(models.Model):
-    
-    _name = 'academy.course'
-    _description = 'Course Info'
+class Book(models.Model):
+
+    _name = 'academy.book'
+    _description = 'Book'
     
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
-    level = fields.Selection(string='Level', selection=[('beginner', 'Beginner'), ('intermediate', 'Intermediate'), ('advanced', 'Advanced')])
     isbn = fields.Char(string='ISBN')
     authors = fields.Char(string='Authors')
     year_of_edition = fields.Char(string='Year of Edition')
     genre = fields.Char(string='Genre')
     editors = fields.Char(string='Editors')
-    
