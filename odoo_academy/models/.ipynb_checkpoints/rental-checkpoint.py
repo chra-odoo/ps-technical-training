e86754d86b3e6217306d9a7ec2f55e59fce9e4a3
chra-odoo <chra@odoo.com>
2021-08-05 18:03:56 +0000

    [ADD] add rental model, views, menuitems, and security

---
+++ odoo_academy/models/.ipynb_checkpoints/rental-checkpoint.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Rental(models.Model):
+    _name = 'rental.rental'
+    _description = 'Book Rental'
+    
+    customer_id = fields.Many2one(comodel_name='res.partner', string='Customer')
+    book_ids = fields.One2many(comodel_name='academy.book', inverse_name='rental_id', string='Books')
